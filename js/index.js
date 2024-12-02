df65294c89b177f05ab69b906f7cfb83d99496ce
Kaio Nilo <kaionilofreitas@gmail.com>
2024-12-02 00:11:10 -0300

    atualizando carrossel do index.html

--- js/index.js
+++ js/index.js
@@ -1,13 +1,1 @@
-let obj = document.querySelectorAll("div.obj")
 
-function aumentarObj(){
-    obj.style.transform = "scale(x, y)"
-}
-
-obj.addEventListener("mouseover", aumentarObj);
-
-function diminuirObj(){
-    obj.style.transform = "none"
-}
-
-obj.addEventListener("mouseout", diminuirObj);
