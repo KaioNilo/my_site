0b455e6848bff19f8cf6bb199063bb49af68a80f
Kaio Nilo <kaionilofreitas@gmail.com>
2024-12-02 20:46:51 -0300

    criaçao do meni hamburguer

--- js/index.js
+++ js/index.js
@@ -1,1 +1,33 @@
+////////////////////////////////////////////////////////////////
+/* MENU HAMBURGER */
+function clickMenu() {
+    let listaMenu = document.getElementById("lista-menu");
 
+    if (listaMenu.style.display == "block") {
+        listaMenu.style.display = "none";
+    } else {
+        listaMenu.style.display = "block";
+    }
+
+}
+
+
+////////////////////////////////////////////////////////////////
+/* CARROSEL */
+
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval( function () {
+    nextImg()
+}, 4500 /* milissegundos */)
+
+// função para passar imgs
+function nextImg() {
+    count++;
+    if ( count > 4) {
+        count = 1;
+    }
+
+    document.getElementById("radio" + count).checked = true;
+}
